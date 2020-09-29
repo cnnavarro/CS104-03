ed07f13edaf678e966b90279868e0092c1098f22
cnnavarro <71734071+cnnavarro@users.noreply.github.com>
2020-09-29 17:51:46 -0400

    Add files via upload

---
+++ Average.py
@@ -0,0 +1,17 @@
+numberOfScores = 0
+score1 = 0
+total = 0
+average = 0.0
+scoreCount = 0
+numberOfScores = int(input ("Please enter the number of scores you want to average:"))
+
+#What python loop structure could we use to repeat the next 3 lines
+score = int (input ("Please enter a score: "))
+total = total + score
+scoreCount = scoreCount + 1
+
+
+average = total / numberOfScores
+print(average)
+
+
