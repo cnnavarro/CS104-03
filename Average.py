babe7b0e08d5c7613adbc611ccbfb4dbfea22e56
cnnavarro <71734071+cnnavarro@users.noreply.github.com>
2020-10-27 16:50:23 -0400

    Update Average.py

--- Average.py
+++ Average.py
@@ -4,9 +4,7 @@ score2 = 0
 total = 0
 average = 0.0
 scoreCount = 0
-#numberOfScores = int(input("Please enter the number of scores that you want to average: "))
 
-#What python loop structure could we use to repeat the next 3 lines
 score = int(input("Please enter the score: "))
 total = total + score
 scoreCount = scoreCount + 1
